34383e06669404d0fabc79df91a9e8159e2f9985
cloliveira-pisom <claudio.oliveira@pisomtech.com.br>
2026-07-10 21:49:28 -0300

    Usa Dockerfile.native-micro oficial do Quarkus no CI

--- docker/postgres/migrations/010_ordem_servico_descricoes.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE ordens_servico
-    ADD COLUMN IF NOT EXISTS descricao_servicos_terceirizados VARCHAR(500);
-
-ALTER TABLE ordens_servico
-    ADD COLUMN IF NOT EXISTS descricao_mao_de_obra VARCHAR(500);

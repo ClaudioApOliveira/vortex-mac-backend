21845cc770c3881d24719cf1a5578795946cf000
cloliveira-pisom <claudio.oliveira@pisomtech.com.br>
2026-07-12 14:01:20 -0300

    ajuste no token response

--- src/main/java/com/vortex/auth/dto/TokenResponse.java
+++ src/main/java/com/vortex/auth/dto/TokenResponse.java
@@ -1,5 +1,8 @@
 package com.vortex.auth.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record TokenResponse(
     String accessToken,
     String tipo,
